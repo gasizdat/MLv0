801ccefe81afc30c4db8a2411ff4b2005acadf9d
gasizdat <gasizdat@gmail.com>
2022-09-16 10:54:44 +0300

    (*) Improve Perceptron

--- MLv0.Net/perceptron.ts
+++ MLv0.Net/perceptron.ts
@@ -1,43 +1,45 @@
-﻿/// <reference path="../MLv0.Core/ievaluatable.ts" />
-/// <reference path="../MLv0.Core/dataset.ts" />
+﻿/// <reference path="types.ts" />
+/// <reference path="../MLv0.Core/ievaluatable.ts" />
 /// <reference path="../MLv0.Utils/assert.ts" />
 
 module MLv0.Net
 {
-    type Signal = number;
-    type Weight = number;
-    type Gain = number;
+    import assert = MLv0.Utils.assert;
 
-    type Inputs = MLv0.Core.DataSubset<Signal>;
-    type Outputs = MLv0.Core.DataSubset<Signal>;
-    type Weights = MLv0.Core.DataSubset<Weight>;
-    type Gains = MLv0.Core.DataSubset<Gain>;
+    export type Inputs = MLv0.Core.Subset<SignalType>;
+    export type Output = MLv0.Core.Subset1<SignalType>;
+    export type Weights = MLv0.Core.Subset<WeightType>;
+    export type Bias = MLv0.Core.Subset1<BiasType>;
 
     export class Perceptron implements MLv0.Core.IEvaluatable
     {
-        constructor(inputs: Inputs, outputs: Outputs, weights: Weights, gains: Gains)
+        constructor(inputs: Inputs, output: Output, weights: Weights, bias: Bias)
         {
-            MLv0.Utils.assert(inputs.length == weights.length);
+            assert(inputs.length == weights.length);
 
             this._inputs = inputs;
-            this._outputs = outputs;
+            this._output = output;
             this._weights = weights;
-            this._gains = gains;
+            this._bias = bias;
         }
 
         public evaluate(): void
         {
-            var sum: number = 0;
+            var sum: number = this._bias.value;
             const weights = this._weights;
             this._inputs.forEachIndex((item, index) => sum += item * weights.get(index));
 
-            const output = sum > this._gains.get(0);
-            this._outputs.setAll(output ? 1.0 : 0.0);
+            this._output.value = (sum >= 0) ? 1.0 : 0.0;
         }
 
-        private _inputs: Inputs;
-        private _outputs: Outputs;
-        private _weights: Weights;
-        private _gains: Gains;
+        public get output(): Output
+        {
+            return this._output;
+        }
+
+        private readonly _inputs: Inputs;
+        private readonly _output: Output;
+        private readonly _weights: Weights;
+        private readonly _bias: Bias;
     }
 }
---
+++ MLv0.Net/types.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../MLv0.Core/ievaluatable.ts" />
+/// <reference path="../MLv0.Core/subset.ts" />
+/// <reference path="../MLv0.Core/subset1.ts" />
+
+module MLv0.Net
+{
+    export type SignalType = number;
+    export type WeightType = number;
+    export type BiasType = number;
+}
