693049de0aa21d6e04acc2a1e529f9b22fcd78c5
gasizdat <gasizdat@gmail.com>
2022-09-16 10:55:01 +0300

    (+) Add Layer

---
+++ MLv0.Net/connectom.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="perceptron.ts" />
+/// <reference path="layer.ts" />
+/// <reference path="types.ts" />
+/// <reference path="../MLv0.Core/ievaluatable.ts" />
+/// <reference path="../MLv0.Core/set.ts" />
+/// <reference path="../MLv0.Utils/assert.ts" />
+
+module MLv0.Net
+{
+    import assert = MLv0.Utils.assert;
+
+    export class Connectom implements MLv0.Core.IEvaluatable
+    {
+        constructor(...population: number[])
+        {
+            assert(population.length > 0);
+            //population.reduce(
+        }
+
+        public evaluate(): void
+        {
+        }
+    }
+}
---
+++ MLv0.Net/layer.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="perceptron.ts" />
+/// <reference path="types.ts" />
+/// <reference path="../MLv0.Core/ievaluatable.ts" />
+/// <reference path="../MLv0.Core/set.ts" />
+/// <reference path="../MLv0.Utils/assert.ts" />
+
+module MLv0.Net
+{
+    import assert = MLv0.Utils.assert;
+
+    type Perceptrons = MLv0.Core.Subset<Perceptron>;
+    type Outputs = MLv0.Core.Subset<SignalType>;
+
+    export class Layer implements MLv0.Core.IEvaluatable
+    {
+        constructor(perceptrons: Perceptrons)
+        {
+            assert(perceptrons.length > 0);
+
+            this._perceptrons = perceptrons;
+        }
+
+        public evaluate(): void
+        {
+            this._perceptrons.forEach(perceptron => perceptron.evaluate());
+        }
+
+        public get outputs(): Outputs
+        {
+            const outputs = new Array<MLv0.Core.Subset1<SignalType>>();
+            this._perceptrons.forEach(perceptron => outputs.push(perceptron.output));
+            return MLv0.Core.Subset.join<SignalType>(outputs);
+        }
+
+        private readonly _perceptrons: Perceptrons;
+    }
+}
