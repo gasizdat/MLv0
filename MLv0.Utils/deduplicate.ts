032329ec064e131f92fc77d107cc47de4e958403
gasizdat <gasizdat@gmail.com>
2022-09-17 11:23:49 +0300

    (+) Add utils methods: deduplicate, range

---
+++ MLv0.Utils/deduplicate.ts
@@ -0,0 +1,8 @@
+﻿
+module MLv0.Utils
+{
+    export function deduplicate<T>(array: T[]): T[]
+    {
+        return Array.from(new Set<T>(array));
+    }
+}
---
+++ MLv0.Utils/range.ts
@@ -0,0 +1,8 @@
+﻿
+module MLv0.Utils
+{
+    export function range(start: number, count: number): number[]
+    {
+        return Array.from({ length: count }, (_, i) => i + start);
+    }
+}
