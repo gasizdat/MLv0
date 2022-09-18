1a3f36567eaf2cdb46305d2a66bb765b9635c676
gasizdat <gasizdat@gmail.com>
2022-09-18 11:46:32 +0300

    (~) Extract input image processing in the separate module.

---
+++ MLv0.UI/inputimage.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path="../MLv0.Utils/ensure.ts" />
+
+module MLv0.UI
+{
+    export class InputImage
+    {
+        public static async getImageDataFromCanvas(canvas: HTMLCanvasElement, width: number, height: number): Promise<number[]>
+        {
+            const async_img = new Image();
+            async_img.src = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            const img = await new Promise<HTMLImageElement>((resolve, reject) =>
+            {
+                async_img.onload = () => resolve(async_img);
+                async_img.onerror = () => reject()
+            });
+
+            const context = MLv0.Utils.ensure(canvas.getContext("2d"));
+            context.save();
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, 0, 0, width, height);
+
+            const img_data = context.getImageData(0, 0, width, height).data;
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, 0, 0, canvas.width, canvas.height);
+            context.restore();
+
+            const ret = new Array<number>(img_data.length / 4);
+            for (var i = 0, j = 0; i < img_data.length; i += 4, j++)
+            {
+                const pix = img_data[i] || img_data[i + 1] || img_data[i + 2] || img_data[i + 3];
+                ret[j] = (pix ? 1.0 : 0.0);
+            }
+            return ret;
+        }
+
+    }
+}
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 ﻿/// <reference path="MLv0.Net/connectom.ts" />
 /// <reference path="MLv0.Utils/ensure.ts" />
+/// <reference path="MLv0.UI/inputimage.ts" />
 
 class Model implements MLv0.Core.IEvaluatable
 {
@@ -12,48 +13,25 @@ class Model implements MLv0.Core.IEvaluatable
         {
             this._connectom.weights.set(i, -10 + Math.random() * 20);
         }
-    }
 
-    public async evaluate(): Promise<void>
-    {
-        const t1 = Date.now();
-        const canvas = this._canvas;
         const context = MLv0.Utils.ensure(canvas.getContext("2d"));
 
-        var cg: CanvasGradient = context.createLinearGradient(0,0,64,64);
+        var cg: CanvasGradient = context.createLinearGradient(0, 0, 64, 64);
         cg.addColorStop(0.10, "blue");
         cg.addColorStop(0.20, "green");
         cg.addColorStop(0.40, "yellow");
         context.lineWidth = 3;
         context.strokeStyle = cg; "rgba(11,27,47,1)";
         context.strokeRect(5, 5, 54, 54);
+    }
 
-        const async_img = new Image();
-        async_img.src = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-        const img = await new Promise<HTMLImageElement>((resolve, reject) =>
-        {
-            async_img.onload = () => resolve(async_img);
-            async_img.onerror = () => reject()
-        });
+    public async evaluate(): Promise<void>
+    {
+        const t1 = Date.now();
+        const image_data = await MLv0.UI.InputImage.getImageDataFromCanvas(this._canvas, this._width, this._height);
 
-        context.save();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(img, 0, 0, this._width, this._height);
+        this._connectom.layers.get(0).inputs.setAll(image_data);
 
-        const img_data = context.getImageData(0, 0, this._width, this._height).data;
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);
-        context.restore();
-
-        const inputs = this._connectom.layers.get(0).inputs;
-        for (var i = 0, j = 0; i < img_data.length; i += 4, j++)
-        {
-            const pix = img_data[i] || img_data[i + 1] || img_data[i + 2] || img_data[i + 3];
-            inputs.set(j, pix ? 1.0 : 0.0);
-        }
-
-        //console.log(bitmap.height);
         this._connectom.evaluate();
         const duration = Date.now() - t1;
 
