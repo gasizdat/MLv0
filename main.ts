4281a6838f993a9f061c71633f4e0ac557dc4488
gasizdat <gasizdat@gmail.com>
2022-10-30 13:46:03 +0400

    (+) Add GA trainer for classifier.

--- main.ts
+++ main.ts
@@ -51,80 +51,156 @@ class Model implements MLv0.Core.IEvaluatable
             { size: 10, transferFunction: MLv0.Core.sigma }
         );
         this._canvas = canvas;
-        this._connectom.biases.setAll(0);
 
-        for (var i = 0; i < this._connectom.weights.length; i++)
+        const genomLength = 20;
+        const weightsGeneration = new Array<MLv0.GA.Genome<MLv0.Net.WeightType>>(genomLength);
+        const biasesGeneration = new Array<MLv0.GA.Genome<MLv0.Net.BiasType>>(genomLength);
+        for (var i = 0; i < genomLength; i++)
         {
-            this._connectom.weights.set(i, -10 + Math.random() * 20);
+            const weights = new Array<MLv0.Net.WeightType>(this._connectom.weights.length);
+            const biases = new Array<MLv0.Net.BiasType>(this._connectom.biases.length);
+
+            for (var j = 0; j < weights.length; j++)
+            {
+                weights[j] = -10 + Math.random() * 20;
+            }
+            for (var j = 0; j < biases.length; j++)
+            {
+                biases[j] = Math.random();
+            }
+
+            weightsGeneration[i] = new MLv0.GA.Genome<MLv0.Net.WeightType>(weights);
+            biasesGeneration[i] = new MLv0.GA.Genome<MLv0.Net.BiasType>(biases);
         }
+        this._weightsGeneration = new MLv0.GA.Generation<MLv0.Net.WeightType>(weightsGeneration);
+        this._biasesGeneration = new MLv0.GA.Generation<MLv0.Net.BiasType>(biasesGeneration);
 
         const $this = this;
         fileInput.addEventListener('change', async (_) =>
         {
-            $this._contentIndex = 0;
-            $this._pictureIndex = 0;
             const contents = await MLv0.UI.InputFile.getContents(fileInput);
             $this._contents = contents.map(content => new MLv0.UI.InputFile(content, this._dataSetWidth, this._dataSetHeight));
 
             console.log($this._contents.length);
-            window.requestAnimationFrame(this.evaluate.bind(this));
+            await this.evaluate();
         });
     }
 
     public async evaluate(): Promise<void>
     {
-        if (this._contents && this._contents.length > this._contentIndex)
+        MLv0.Utils.assert(this._weightsGeneration.genomes.length == this._biasesGeneration.genomes.length);
+
+        for (var i = 0; i < this._weightsGeneration.genomes.length; i++)
         {
-            const content = this._contents[this._contentIndex];
-            if (content.length > this._pictureIndex)
+            const weightsGenome = this._weightsGeneration.genomes[i];
+            const biasesGenome = this._biasesGeneration.genomes[i];
+            const connectom = new MLv0.Net.Connectom(
+                { size: this._sensorHeight * this._sensorWidth, transferFunction: MLv0.Core.heaviside },
+                { size: 51, transferFunction: MLv0.Core.heaviside },
+                { size: 31, transferFunction: MLv0.Core.heaviside },
+                { size: 10, transferFunction: MLv0.Core.sigma });
+
+            connectom.weights.setAll(weightsGenome.data);
+            connectom.biases.setAll(biasesGenome.data);
+            const assessment = await this.trainClassifier(connectom);
+            console.log(`Assessment: ${assessment}`);
+        }
+    }
+
+    public async trainClassifier(connectom: MLv0.Net.Connectom): Promise<number>
+    {
+        MLv0.Utils.assert(this._contents);
+        var currentAssessment = 0;
+        var sampleCount = 0;
+        for (const content of this._contents)
+        {
+            for (var sampleNo = 0; sampleNo < content.length; sampleNo++)
             {
-                const t1 = Date.now();
+                //const t1 = Date.now();
                 const draw_scale = this._dataScale.value;
-                const source = content.getSample(this._pictureIndex);
+                const sample = content.getSample(sampleNo);
                 const scaled_image = MLv0.UI.InputImage.scale(
-                    source.bitmap,
+                    sample.bitmap,
                     content.width,
                     content.height,
                     draw_scale
                 );
 
-                await MLv0.UI.InputImage.draw(
-                    this._canvas,
-                    scaled_image.bitmap,
-                    scaled_image.width,
-                    scaled_image.height
-                );
-
                 const sensor_scale = this._sensorWidth / content.width;
                 MLv0.Utils.assert(sensor_scale == this._sensorHeight / content.height)
                 const input_image = MLv0.UI.InputImage.scale(
-                    source.bitmap,
+                    sample.bitmap,
                     content.width,
                     content.height,
                     sensor_scale
                 );
 
-                this._connectom.layers.get(0).inputs.setAll(input_image.bitmap);
+                Model.getInputs(connectom).setAll(input_image.bitmap);
+                connectom.evaluate();
+                currentAssessment += Model.fitnessFunction(Model.getOutputs(connectom), sample.value);
+                MLv0.Utils.assert(isFinite(currentAssessment));
+                MLv0.Utils.assert(!isNaN(currentAssessment));
 
-                this._connectom.evaluate();
-                const duration = Date.now() - t1;
+                /*const duration = Date.now() - t1;
 
-                const outputs = this._connectom.layers.get(this._connectom.layers.length - 1).outputs;
-                outputs.forEachIndex((output, index) => console.log((index) + ": " + (output * 100).toFixed(2) + ", "));
-                console.log(`Duration ${duration}, actual: ${source.value}`);
-                this._pictureIndex++;
+                this.outputs.forEachIndex((output, index) => console.log((index) + ": " + (output * 100).toFixed(2) + ", "));
+                console.log(`Duration ${duration}, actual: ${sample.value}`);*/
+
+                if (((sampleCount++) % 300) == 0)
+                {
+                    await this.drawOnCanvas(scaled_image);
+                }
+                this._dataScale.evaluate();
             }
-            else
-            {
-                this._pictureIndex = 0;
-                this._contentIndex++;
-            }
-            this._dataScale.evaluate();
         }
 
-        const duration = 0;
-        const $this = this;
-        setTimeout(() => window.requestAnimationFrame($this.evaluate.bind($this)), duration);
+        console.log(`Samples processed: ${sampleCount}, Current assessment: ${currentAssessment}`);
+        return currentAssessment;
+    }
+
+    public get inputs()
+    {
+        return Model.getInputs(this._connectom);
+    }
+    public get outputs()
+    {
+        return Model.getOutputs(this._connectom);
+    }
+
+    protected static getInputs(connectom: MLv0.Net.Connectom)
+    {
+        return connectom.layers.get(0).inputs;
+    }
+    protected static getOutputs(connectom: MLv0.Net.Connectom)
+    {
+        return connectom.layers.get(connectom.layers.length - 1).outputs;
+    }
+    protected async drawOnCanvas(scaled_image: { bitmap: number[], width: number, height: number }): Promise<void>
+    {
+        const canvas = this._canvas;
+        window.requestAnimationFrame(() =>
+        {
+            MLv0.UI.InputImage.draw(
+                canvas,
+                scaled_image.bitmap,
+                scaled_image.width,
+                scaled_image.height
+            );
+        });
+
+        //reschedule other async function invocations
+        return new Promise(f => setTimeout(f, 0));
+    }
+
+    protected static fitnessFunction(outputs: MLv0.Core.Subset<MLv0.Net.SignalType>, value: number): number
+    {
+        var result = 0;
+        outputs.forEachIndex((output, index) =>
+        {
+            const rate = (1 - output + 1e-50);
+            result += (index == value) ? rate : (-rate);
+        });
+        return result;
     }
 
     private readonly _sensorHeight = 12;
@@ -134,75 +210,15 @@ class Model implements MLv0.Core.IEvaluatable
     private readonly _dataScale = new MotionZoom(2, 1.013);
     private readonly _canvas: HTMLCanvasElement;
     private readonly _connectom: MLv0.Net.Connectom;
+    private readonly _weightsGeneration: MLv0.GA.Generation<MLv0.Net.WeightType>;
+    private readonly _biasesGeneration: MLv0.GA.Generation<MLv0.Net.BiasType>;
     private _contents?: MLv0.UI.InputFile[];
-    private _contentIndex = 0;
-    private _pictureIndex = 0;
 }
 
 var model: Model;
 
-function generationTest()
+window.onload = async () =>
 {
-    const generation = new MLv0.GA.Generation<string>([
-        [
-            new MLv0.GA.Gen("A"),
-            new MLv0.GA.Gen("B"),
-            new MLv0.GA.Gen("C"),
-            new MLv0.GA.Gen("D")
-        ],
-        [
-            new MLv0.GA.Gen("E"),
-            new MLv0.GA.Gen("F"),
-            new MLv0.GA.Gen("G"),
-            new MLv0.GA.Gen("H")
-        ],
-        [
-            new MLv0.GA.Gen("I"),
-            new MLv0.GA.Gen("K"),
-            new MLv0.GA.Gen("L"),
-            new MLv0.GA.Gen("M")
-        ],
-        [
-            new MLv0.GA.Gen("X"),
-            new MLv0.GA.Gen("Y"),
-            new MLv0.GA.Gen("Z"),
-            new MLv0.GA.Gen("W")
-        ]
-    ]);
-
-    generation.setRank(generation.genomes[3], 3);
-    generation.setRank(generation.genomes[2], 4);
-    generation.setRank(generation.genomes[1], 5);
-    generation.setRank(generation.genomes[0], 2);
-
-    const new_generation = generation.newGeneration((a, b) =>
-    {
-        if ((Math.random() * 3) > 1.5)
-        {
-            return a;
-        }
-        else
-        {
-            return b;
-        }
-    }, (value) =>
-    {
-        if ((Math.random() * 17) > 15.7)
-        {
-            return value += "m";
-        }
-        else
-        {
-            return value;
-        }
-    });
-    console.log(new_generation);
-}
-
-window.onload = () =>
-{
-    generationTest();
-
     const canvas = document.getElementById('playArea') as HTMLCanvasElement;
     const file = document.getElementById('localFile') as HTMLInputElement;
     model = new Model(canvas, file);
